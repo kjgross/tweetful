61e27b30df475c79a157da3e5fc1d02687b8f653
Kelsey <kelsey@edmodo.com>
2014-09-29 20:22:03 -0700

    add one API call that works

--- tweetful.py
+++ tweetful.py
@@ -1,9 +1,34 @@
 import authorization
+import sys
+import requests
+import json
+from urls import *
+
+
+def get_timeline():
+##This one works
+	auth = authorization.authorize()
+	response = requests.get(TIMELINE_URL, auth=auth)
+	print json.dumps(response.json(), indent=4)
+
+def get_mentions():
+## This one doesn't
+	auth = authorization.authorize()
+	response = requests.get(MENTIONS_URL, auth=auth)
+	print json.dumps(response.json(), indent=4)
+
 
 
 def main():
-    """ Main function """
-    authorization.authorize()
+	""" Main function """
+
+	#get_timeline()
+	get_mentions()
+
+
+
+
 
 if __name__ == "__main__":
-    main()
+	print "calling?"
+	main()
