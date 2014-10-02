92e27cd518cb6feb76d0277f017ba432e5958ba0
Kelsey <kelsey@edmodo.com>
2014-10-01 20:50:38 -0700

    Add comments

--- tweetful.py
+++ tweetful.py
@@ -11,18 +11,28 @@ logging.basicConfig(filename="output.log", level=logging.DEBUG)
 
 
 def get_timeline():
-##This one works
 	logging.debug("Get Timeline")
 	auth = authorization.authorize()
 	response = requests.get(TIMELINE_URL, auth=auth)
-	print json.dumps(response.json(), indent=4)
+	return json.dumps(response[text].json(), indent=4)
 
 def get_mentions():
-## This one doesn't
 	logging.debug("Get Mentions")
 	auth = authorization.authorize()
 	response = requests.get(MENTIONS_URL, auth=auth)
-	print json.dumps(response.json(), indent=4)
+##This guy should only print the "text" part, and leave out the rest of the info.. but it tdoesn't.
+	#for t in response:
+		#print t.text.encode('utf8')
+		#print t.user.screen_name
+##This guy prints everything.
+	return json.dumps(response.json(), indent=4)
+
+def search_for(term):
+	logging.debug("Search for...")
+	auth = authorization.authorize()
+	response = requests.get(SEARCH_URL, term=term, auth=auth)
+	for t in response:
+		print t.text.encode('utf-8')
 
 def make_parser():
 	"""Construct the command line parser """
@@ -35,12 +45,16 @@ def make_parser():
 	# Subparser for the get timeline command
 	logging.debug("Constructing get_timeline subparser")
 	timeline_parser = subparsers.add_parser("timeline", help = "Get your Timeline")
-	#put_parser.add_argument("name", help="The name of the snippet")
-	#put_parser.add_argument("snippet", help="The snippet")
-	#put_parser.add_argument("filename", default="snippets.csv", nargs="?", help="The snippet filename")
 
+	# Subparser for the get mentions command
 	logging.debug("Constructing get_mentions subparser")
 	mentions_parser = subparsers.add_parser("mentions", help = "Get your mentions")
+	mentions_parser.add_argument("text", nargs="?", help="The text of the tweet")
+	
+	# Subparser for the search command
+	logging.debug("Constructing search_for subparser")
+	search_parser = subparsers.add_parser("search", help = "Search for a specific tweet text")
+	search_parser.add_argument("term", help="The term you are searching for")
 
 	return parser
 
@@ -64,6 +78,9 @@ def main():
 	elif command == "mentions":
 		my_mentions = get_mentions()
 		print "Getting mentions info '{}'".format(my_mentions)
+	elif command == "search":
+		my_search = search_for(**arguments)
+		print "Getting search info '{}'".format(my_search)
 
 
 
